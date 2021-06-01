dd731add9ce80619d7567e23934cb802d6e8e17a
Pandurang Sadgir <psadgir111@gmail.com>
2021-06-01 21:59:03 +0530

    star-hollow-square pattern with diagonal

---
+++ star-hollow-square-withdiagonal.js
@@ -0,0 +1,22 @@
+/*
+Star hollow square with diagonal pattern
+
+*/
+
+let star = "";
+let n = 9;
+for(let i=1; i<=n; i++){// for new line
+
+    for(let j=1;j<=n;j++){//for star printing
+        if(i==1|i==n|j==1|j==n|i==j|j==n-i+1){
+            star = star+"*"; //for stars
+
+        }else{
+            star = star+" " //for spaces
+        }
+
+    }
+    star =star+"\n"
+
+}
+console.log(star);
