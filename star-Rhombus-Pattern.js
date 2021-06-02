5ec9e0a76dbff7ea700aecfc52d390232a265b06
Pandurang Sadgir <psadgir111@gmail.com>
2021-06-02 08:29:39 +0530

    star-rombus-pattern

---
+++ star-Rhombus-Pattern.js
@@ -0,0 +1,17 @@
+/*
+
+*/
+let star = "";
+let n = 7;
+for(let i=1;i<=n;i++){//for rows
+    for(let k=1;k<=6-i+1; k++){ //for spaces
+        star= star+" ";
+
+    }
+    for(let j=1;j<=n;j++){
+        star=star+"*";
+    }
+    star=star+"\n"
+
+}
+console.log(star);
