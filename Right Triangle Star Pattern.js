ab1a4f8c8755759691fa47ec2923961ef8929ade
Pandurang Sadgir <psadgir111@gmail.com>
2021-06-03 08:10:08 +0530

    Right Triangle star pattern

---
+++ Right Triangle Star Pattern.js
@@ -0,0 +1,14 @@
+/*
+Right Triangle Star Pattern.
+
+ */
+let star = "";
+for(let i=1; i<=9; i++){//for new line
+    for(let j=1;j<=i; j++){
+        star=star+"*";
+
+    }
+    star=star +"\n";
+
+}
+console.log(star);
